8b373345d10b557400629ee1f95fd1211f64bfb0
Jannae Jacks <jannae@gmail.com>
2015-03-27 16:07:37 -0400

    Updating sublime installation script

--- apps/sublime/go.sh
+++ apps/sublime/go.sh
@@ -16,10 +16,7 @@ curl -o Package\ Control.sublime-package https://packagecontrol.io/Package%20Con
 cp -r Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package 2> /dev/null
 rwq()
 
-# Install Sublime Text Package Manager settings
-cp -r Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Package\ Control.sublime-settings 2> /dev/null
-rwq()
-
-# Install Sublime Text settings
-cp -r Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
+# Link Sublime Text User Settings
+rm -rf ~/Library/Application\ Support/Sublime\ Text*/Packages/User
+ln -s User ~/Library/Application\ Support/Sublime\ Text*/Packages/
 rwq()
