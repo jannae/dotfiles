fcf76d3caea79151583fbba68732db45ead11d81
Jannae Jacks <jannae@gmail.com>
2015-10-14 11:58:59 -0400

    install-bins: machine setup continues to evolve

--- setup/install-bins.sh
+++ setup/install-bins.sh
@@ -1,3 +1,27 @@
+##################################
+### XCode Command Line Tools
+#      thx  https://github.com/alrra/dotfiles/blob/c2da74cc333/os/os_x/install_applications.sh#L39
+
+if [ $(xcode-select -p &> /dev/null; printf $?) -ne 0 ]; then
+    xcode-select --install &> /dev/null
+    # Wait until the XCode Command Line Tools are installed
+    while [ $(xcode-select -p &> /dev/null; printf $?) -ne 0 ]; do
+        sleep 5
+    done
+    xcode-select -p &> /dev/null
+    if [ $? -eq 0 ]; then
+        # Prompt user to agree to the terms of the Xcode license
+        # https://github.com/alrra/dotfiles/issues/10
+       sudo xcodebuild -license
+   fi
+fi
+###
+##################################
+
+##########################
+#   Homebrew
+##########################
+
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
@@ -90,10 +114,10 @@ mybinaries=(
     libyaml
     mackup
     mad
-    maven30
     mercurial
     mp3gain
     mtr                 # mtr - ping & traceroute. best.
+    mvnvm
     netpbm
     node
     openssl
@@ -109,7 +133,7 @@ mybinaries=(
     sqlite
     terminal-notifier
     thefuck
-    tidy
+    tidy-html5          # tidy > tidy-html5 <https://github.com/Homebrew/homebrew/pull/39010>
     tig
     trash
     tree
@@ -146,6 +170,18 @@ brew install homebrew/binary/jsl
 # Cleanup!
 brew cleanup
 
+##########################
+#   Post Homebrew Things
+##########################
+
+# change to bash 4 (installed by homebrew)
+BASHPATH=$(brew --prefix)/bin/bash
+if [ $(cat /private/etc/shells | grep "$BASHPATH" | wc -l) -eq 0 ]; then
+    sudo bash -c "echo $BASHPATH >> /private/etc/shells"
+fi
+chsh -s $BASHPATH # will set for current user only.
+echo $BASH && echo $BASH_VERSION # should be 4.x not the old 3.2.X
+
 
 ##########################
 #   PHP things
@@ -168,22 +204,45 @@ pear install --alldeps phpmd/PHP_PMD
 pip install --upgrade setuptools
 pip install --upgrade pip
 
-mypips=(
+# Some global packages I need
+mypys=(
     virtualenv
     virtualenvwrapper
     pylint
 )
 
 echo "Installing my brew binaries..."
-pip install ${mypips[@]}
+pip install ${mypys[@]}
 
 
 ##########################
 #   Node things
 ##########################
+
 mynpms=(
     git-open
 )
 
 echo "Installing my global npm packages..."
 npm install -g ${mynpms[@]}
+
+##########################
+#   Ruby things
+##########################
+
+# Install rvm
+\curl -sSL https://get.rvm.io | bash -s -- --ignore-dotfiles
+
+# rvm install ruby-head
+rvm install 2.2; rvm use 2.2 --default
+
+# test install
+ruby -v; which ruby
+
+mygems=(
+    scss-lint
+    bundler
+)
+
+echo "Installing my ruby gems..."
+gem install ${mygems[@]}
